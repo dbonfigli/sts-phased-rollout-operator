caaf372c032ddca270b9bbd1082ade4596d53382
Diego Bonfigli <d.bonfigli@gmail.com>
2023-01-29 12:45:55 +0100

    add way to disable webhooks for development testing

--- main.go
+++ main.go
@@ -97,9 +97,12 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "PhasedRollout")
 		os.Exit(1)
 	}
-	if err = (&stsplusv1alpha1.PhasedRollout{}).SetupWebhookWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create webhook", "webhook", "PhasedRollout")
-		os.Exit(1)
+
+	if os.Getenv("ENABLE_WEBHOOKS") != "false" {
+		if err = (&stsplusv1alpha1.PhasedRollout{}).SetupWebhookWithManager(mgr); err != nil {
+			setupLog.Error(err, "unable to create webhook", "webhook", "PhasedRollout")
+			os.Exit(1)
+		}
 	}
 	//+kubebuilder:scaffold:builder
 
